35f80038bd33d00edb2929a7e0febd505910fff7
Paulo Cruz <peagafer@gmail.com>
2025-08-23 20:28:33 +0100

    Melhorias no form de sign-up e footer + css

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,6 +1,6 @@
 class WorkersController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
-  
+
   def show
     # TEMP: mock until DB is ready
     @worker = WorkerProfile.includes(:services, :user).find(params[:id])
