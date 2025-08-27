f0e7df47d6235ed88e8337cce947e14afe59c482
Aneli Abe <abe.aneli@gmail.com>
2025-08-26 21:22:06 -0300

    added options for service type to the service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,8 @@ class Service < ApplicationRecord
   belongs_to :category
   has_many :worker_services, dependent: :destroy
   has_many :worker_profiles, through: :worker_services
+
+  enum service_type: { remote: "remoto", on_site: "presencial", establishment: "estabelecimento" }
+
+  validates :service_type, presence: true, inclusion: { in: service_types.keys }
 end
